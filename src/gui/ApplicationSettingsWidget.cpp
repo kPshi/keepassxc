64367d2343387a787dbed37f7eed13bee346d0d0
Jonathan White <support@dmapps.us>
2020-04-06 08:42:20 -0400

    Significantly enhance hardware key robustness
    * Significantly improve user experience when using hardware keys on databases in both GUI and CLI modes. Prevent locking up the YubiKey USB interface for prolonged periods of time. Allows for other apps to use the key concurrently with KeePassXC.
    
    * Improve messages displayed to user when finding keys and when user interaction is required. Output specific error messages when handling hardware keys during database read/write.
    
    * Only poll for keys when previously used or upon user request. Prevent continuously polling keys when accessing the UI such as switching tabs and minimize/maximize.
    
    * Add support for using multiple hardware keys simultaneously. Keys are identified by their serial number which prevents using the wrong key during open and save operations.
    
    * Fixes #4400
    * Fixes #4065
    * Fixes #1050
    * Fixes #1215
    * Fixes #3087
    * Fixes #1088
    * Fixes #1869

--- src/gui/ApplicationSettingsWidget.cpp
+++ src/gui/ApplicationSettingsWidget.cpp
@@ -390,6 +390,7 @@ void ApplicationSettingsWidget::saveSettings()
 
     if (!config()->get(Config::RememberLastKeyFiles).toBool()) {
         config()->remove(Config::LastKeyFiles);
+        config()->remove(Config::LastChallengeResponse);
         config()->remove(Config::LastDir);
     }
 
